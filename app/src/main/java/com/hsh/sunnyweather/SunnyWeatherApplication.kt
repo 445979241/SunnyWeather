00653fded04b07a964d08c95451a4b156cc85957
445979241 <www.hsh445979241@qq.com>
2023-08-27 19:33:20 +0800

    add application

---
+++ app/src/main/java/com/hsh/sunnyweather/SunnyWeatherApplication.kt
@@ -0,0 +1,12 @@
+package com.hsh.sunnyweather
+
+class SunnyWeatherApplication : Application() {
+    companion object {
+        @SuppressLint("StaticFieldLeak")
+        lateinit var context: Context
+    }
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+    }
+}
